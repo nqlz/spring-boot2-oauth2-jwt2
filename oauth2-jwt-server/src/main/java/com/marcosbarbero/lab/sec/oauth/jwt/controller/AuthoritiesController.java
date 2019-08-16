ea24f8374b1f5823cadafb9c92eb6e9bdd860fa8
zoutao <1689613805@qq.com>
2019-08-16 17:09:16 +0800

    add users custom

---
+++ oauth2-jwt-server/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/controller/AuthoritiesController.java
@@ -0,0 +1,20 @@
+package com.marcosbarbero.lab.sec.oauth.jwt.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author MR.zt
+ * @since 2019-08-16
+ */
+@RestController
+@RequestMapping("/authorities")
+public class AuthoritiesController {
+
+}
